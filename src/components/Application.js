5dfa63d24a561c932bd3f59b65e9fe54e884e1b5
Lalafang33 <lalafang33@gmail.com>
2022-10-06 17:03:41 -0700

    comitting first axios days replacement of hardcode

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Appointment from "components/Appointment";
+import axios from "axios";
 
 
 import "components/Application.scss";
@@ -46,28 +47,19 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
 
-  const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState([])
   const appointmentArr = Object.values(appointments).map(app =>{
     return <Appointment key={app.id} {...app} />
   })
+
+  useEffect(()=>{
+    axios.get("/api/days")
+    .then(response => {
+      console.log(response[0])
+      setDay(response[0]);
+    })
+  }, [])
 
   return (
     <main className="layout">
